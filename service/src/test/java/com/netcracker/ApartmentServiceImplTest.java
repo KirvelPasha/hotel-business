2a886497d1f346e6bb42d64f9013f80f09a7aec4
kapusta-elizaveta <kapustaeliz@gmail.com>
2020-04-22 21:02:00 +0300

    add getAllTest

---
+++ service/src/test/java/com/netcracker/ApartmentServiceImplTest.java
@@ -0,0 +1,55 @@
+package com.netcracker;
+
+import com.netcracker.converter.ApartmentConverter;
+import com.netcracker.dto.ApartmentDto;
+import com.netcracker.entity.Apartment;
+import com.netcracker.repository.ApartmentRepository;
+import com.netcracker.service.ApartmentService;
+import com.netcracker.serviceimpl.ApartmentServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+public class ApartmentServiceImplTest {
+
+    @Mock
+    private Apartment apartment;
+    @Mock
+    private ApartmentDto apartmentDto;
+    @Mock
+    private ApartmentConverter apartmentConverter;
+
+    private List<Apartment> apartmentList;
+    private  List<ApartmentDto> apartmentDtoList;
+
+    @Mock
+    private ApartmentRepository apartmentRepository;
+
+    @InjectMocks
+    private ApartmentService apartmentService;
+
+    @Before
+    public void init() {
+        apartmentList = Stream.of(apartment).collect(Collectors.toList());
+        apartmentDtoList = Stream.of(apartmentDto).collect(Collectors.toList());
+    }
+
+    @Test
+    public void getAllTest() {
+        when(apartmentRepository.findAll()).thenReturn(apartmentList);
+        when(apartmentConverter.converter(apartment)).thenReturn(apartmentDto);
+        assertEquals(apartmentDtoList, apartmentService.getAll());
+
+    }
+}
