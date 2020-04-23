17d781a09e6c744e02e7023666420c4e0a071336
kapusta-elizaveta <kapustaeliz@gmail.com>
2020-04-23 16:50:19 +0300

    add apartmentTypeTest

--- service/src/test/java/com/netcracker/ApartmentServiceImplTest.java
+++ service/src/test/java/com/netcracker/ApartmentServiceImplTest.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -30,13 +31,31 @@ public class ApartmentServiceImplTest {
     private Apartment apartment2;
     @Mock
     private Apartment apartment3;
+
     @Mock
-    private ApartmentDto apartmentDto;
+    private ApartmentDto apartmentDto1;
+    @Mock
+    private ApartmentDto apartmentDto2;
+    @Mock
+    private ApartmentDto apartmentDto3;
+
     @Mock
     private ApartmentConverter apartmentConverter;
 
     private List<Apartment> apartmentList;
     private  List<ApartmentDto> apartmentDtoList;
+
+    private List<Apartment>  apartmentListCheaper;
+    private List<ApartmentDto>  apartmentListDtoCheaper;
+
+    private  List<Apartment> apartmentListByCountRoom;
+    private List<ApartmentDto>  apartmentListDtoByCountRoom;
+
+    private List<Apartment>  apartmentListByCountPlaces;
+    private List<ApartmentDto>  apartmentListDtoByCountPlaces;
+
+    private List<Apartment>  apartmentListByCountPlacesAndRoom;
+    private List<ApartmentDto>  apartmentListDtoByCountPlacesAndRoom;
 
     @Mock
     private ApartmentRepository apartmentRepository;
@@ -47,22 +66,110 @@ public class ApartmentServiceImplTest {
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
-        apartmentList = Stream.of(apartment).collect(Collectors.toList());
-        apartmentDtoList = Stream.of(apartmentDto).collect(Collectors.toList());
+        apartmentList = Stream.of(apartment1, apartment2, apartment3).collect(Collectors.toList());
+        apartmentDtoList = Stream.of(apartmentDto1, apartmentDto2, apartmentDto3).collect(Collectors.toList());
+        apartmentListCheaper = Stream.of(apartment1, apartment2).collect(Collectors.toList());
+        apartmentListDtoCheaper = Stream.of(apartmentDto1, apartmentDto2).collect(Collectors.toList());
+        apartmentListByCountRoom = Stream.of(apartment1, apartment3).collect(Collectors.toList());
+        apartmentListDtoByCountRoom = Stream.of(apartmentDto1, apartmentDto3).collect(Collectors.toList());
+        apartmentListByCountPlaces = Stream.of(apartment1, apartment3).collect(Collectors.toList());
+        apartmentListDtoByCountPlaces = Stream.of(apartmentDto1, apartmentDto3).collect(Collectors.toList());
+        apartmentListByCountPlacesAndRoom = Stream.of(apartment1, apartment3).collect(Collectors.toList());
+        apartmentListDtoByCountPlacesAndRoom = Stream.of(apartmentDto1, apartmentDto3).collect(Collectors.toList());
 
-        setupApartment(apartment1, 1);
-        setupApartment(apartment2, 2);
-        setupApartment(apartment3, 3);
+        setupApartment(apartment1, 1, 140, 2, 5);
+        setupApartment(apartment2, 2, 100, 3, 6);
+        setupApartment(apartment3, 3, 200, 2, 5);
+
+        setupApartmentDto(apartmentDto1, 1, 140, 2, 5);
+        setupApartmentDto(apartmentDto2, 2, 100, 3, 6);
+        setupApartmentDto(apartmentDto3, 3, 200, 2, 5);
     }
 
     @Test
     public void getAllTest() {
         when(apartmentRepository.findAll()).thenReturn(apartmentList);
-        when(apartmentConverter.converter(apartment)).thenReturn(apartmentDto);
+        setupConverterApartment(apartment1, apartmentDto1);
+        setupConverterApartment(apartment2, apartmentDto2);
+        setupConverterApartment(apartment3, apartmentDto3);
         assertEquals(apartmentDtoList, apartmentService.getAll());
     }
 
-    private void setupApartment(Apartment apartment, Integer id) {
+    @Test
+    public void getByIdTest() {
+        when(apartmentRepository.findById(1)).thenReturn(Optional.of(apartment1));
+        setupConverterApartment(apartment1, apartmentDto1);
+        assertEquals(apartmentDto1, apartmentService.getById(1));
+    }
+
+    @Test
+    public void getCheaperApartmentsTest() {
+        when(apartmentRepository.getCheaperApartments(150)).thenReturn(apartmentListCheaper);
+        setupConverterApartment(apartment1, apartmentDto1);
+        setupConverterApartment(apartment2, apartmentDto2);
+        assertEquals(apartmentListDtoCheaper, apartmentService.getCheaperApartments(150));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getCheaperApartmentsTestException() {
+        apartmentService.getCheaperApartments(-120);
+    }
+
+    @Test
+    public void getApartmentsByCountRoomsTest() {
+        when(apartmentRepository.getApartmentsByCountRooms(2)).thenReturn(apartmentListByCountRoom);
+        setupConverterApartment(apartment1, apartmentDto1);
+        setupConverterApartment(apartment3, apartmentDto3);
+        assertEquals(apartmentListDtoByCountRoom, apartmentService.getApartmentsByCountRooms(2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getApartmentsByCountRoomsTestException() {
+        apartmentService.getApartmentsByCountRooms(-120);
+    }
+
+    @Test
+    public void getApartmentsByCountPlacesTest(){
+        when(apartmentRepository.getApartmentsByCountPlaces(5)).thenReturn(apartmentListByCountPlaces);
+        setupConverterApartment(apartment1, apartmentDto1);
+        setupConverterApartment(apartment3, apartmentDto3);
+        assertEquals(apartmentListDtoByCountPlaces, apartmentService.getApartmentsByCountPlaces(5));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getApartmentsByCountPlacesTestException() {
+        apartmentService.getApartmentsByCountPlaces(-5);
+    }
+
+    @Test
+    public void getApartmentsByCountPlacesAndCountRoomsTest(){
+        when(apartmentRepository.getApartmentsByCountPlacesAndCountRooms(5, 2))
+                .thenReturn(apartmentListByCountPlacesAndRoom);
+        setupConverterApartment(apartment1, apartmentDto1);
+        setupConverterApartment(apartment3, apartmentDto3);
+        assertEquals(apartmentListDtoByCountPlaces, apartmentService.getApartmentsByCountPlacesAndCountRooms(5, 2));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getApartmentsByCountPlacesAndCountRoomTestException() {
+        apartmentService.getApartmentsByCountPlacesAndCountRooms(-5, -6);
+    }
+
+    private void setupApartment(Apartment apartment, Integer id, int price, int countRoom, int countPlaces) {
         when(apartment.getId()).thenReturn(id);
+        when(apartment.getPrice()).thenReturn(price);
+        when(apartment.getCountRooms()).thenReturn(countRoom);
+        when(apartment.getCountPlaces()).thenReturn(countPlaces);
+    }
+
+    private void setupApartmentDto(ApartmentDto apartmentDto, Integer id, int price, int countRoom, int countPlaces) {
+        when(apartmentDto.getId()).thenReturn(id);
+        when(apartmentDto.getPrice()).thenReturn(price);
+        when(apartmentDto.getCountRooms()).thenReturn(countRoom);
+        when(apartmentDto.getCountPlaces()).thenReturn(countPlaces);
+    }
+
+    private void setupConverterApartment(Apartment apartment, ApartmentDto apartmentDto) {
+        when(apartmentConverter.converter(apartment)).thenReturn(apartmentDto);
     }
 }
---
+++ service/src/test/java/com/netcracker/ApartmentTypeServiceImplTest.java
@@ -0,0 +1,102 @@
+package com.netcracker;
+
+import com.netcracker.converter.ApartmentTypeConverter;
+import com.netcracker.dto.ApartmentTypeDto;
+import com.netcracker.entity.ApartmentTypes;
+import com.netcracker.repository.ApartmentTypeRepository;
+import com.netcracker.serviceimpl.ApartmentTypeServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ApartmentTypeServiceImplTest {
+
+    @Mock
+    private ApartmentTypes apartmentTypes1;
+    @Mock
+    private ApartmentTypes apartmentTypes2;
+
+    @Mock
+    private ApartmentTypeDto apartmentTypeDto1;
+    @Mock
+    private ApartmentTypeDto apartmentTypeDto2;
+
+    @Mock
+    private ApartmentTypeConverter apartmentTypeConverter;
+
+    @Mock
+    private ApartmentTypeRepository apartmentTypeRepository;
+
+    private List<ApartmentTypes> apartmentTypesList;
+    private List<ApartmentTypeDto> apartmentTypeDtoList;
+
+    @InjectMocks
+    private ApartmentTypeServiceImpl apartmentTypeService;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+        apartmentTypesList = Stream.of(apartmentTypes1, apartmentTypes2).collect(Collectors.toList());
+        apartmentTypeDtoList = Stream.of(apartmentTypeDto1, apartmentTypeDto2).collect(Collectors.toList());
+
+        setupApartmentType(apartmentTypes1, 1, "vip");
+        setupApartmentType(apartmentTypes2,2, "vip");
+
+        setupApartmentTypeDto(apartmentTypeDto1, 1, "vip");
+        setupApartmentTypeDto(apartmentTypeDto2,2, "vip");
+    }
+
+    @Test
+    public void getAllTest() {
+        when(apartmentTypeRepository.findAll()).thenReturn(apartmentTypesList);
+        setupConverterApartmentType(apartmentTypes1, apartmentTypeDto1);
+        setupConverterApartmentType(apartmentTypes2, apartmentTypeDto2);
+        assertEquals(apartmentTypeDtoList, apartmentTypeService.getAll());
+    }
+
+    @Test
+    public void getByIdTest(){
+        when(apartmentTypeRepository.findById(1)).thenReturn(Optional.of(apartmentTypes1));
+        setupConverterApartmentType(apartmentTypes1, apartmentTypeDto1);
+        assertEquals(apartmentTypeDto1, apartmentTypeService.getById(1));
+    }
+
+    @Test
+    public void saveTest(){
+        when(apartmentTypeRepository.save(apartmentTypes1)).thenReturn(apartmentTypes1);
+        setupConverterApartmentType(apartmentTypes1, apartmentTypeDto1);
+        assertEquals(apartmentTypeDto1, apartmentTypeService.save(apartmentTypeDto1));
+    }
+
+    @Test
+    public void deleteByIdTest(){
+        when(apartmentTypeRepository.findById(1)).thenReturn(Optional.of(apartmentTypes1));
+        apartmentTypeService.deleteById(1);
+        verify(apartmentTypeRepository, times(1)).deleteById(1);
+    }
+
+
+    private void setupApartmentType(ApartmentTypes apartmentTypes, Integer id, String type) {
+        when(apartmentTypes.getId()).thenReturn(id);
+        when(apartmentTypes.getType()).thenReturn(type);
+    }
+
+    private void setupApartmentTypeDto(ApartmentTypeDto apartmentTypeDto, Integer id, String type) {
+        when(apartmentTypeDto.getId()).thenReturn(id);
+        when(apartmentTypeDto.getType()).thenReturn(type);
+    }
+
+    private void setupConverterApartmentType(ApartmentTypes apartmentTypes, ApartmentTypeDto apartmentTypeDto) {
+        when(apartmentTypeConverter.converter(apartmentTypes)).thenReturn(apartmentTypeDto);
+    }
+}
