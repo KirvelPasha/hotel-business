9a23dc3c15fe6888e7923b3e7f2ebc382ae6c739
kapusta-elizaveta <kapustaeliz@gmail.com>
2020-04-26 16:55:48 +0300

    add pathvariable

--- web/src/main/java/com/netcracker/controller/CommentController.java
+++ web/src/main/java/com/netcracker/controller/CommentController.java
@@ -46,8 +46,8 @@ public class CommentController {
     }
 
     @ApiOperation(value = "Gets all comments by apartment's id ")
-    @GetMapping(value = "/filterbyapartmentid")
-    public ResponseEntity<List<CommentDto>> getAllByApartment_Id(@RequestParam("apartmentid") Integer apartmentId) {
+    @GetMapping(value = "/filterbyapartmentid/{apartmentid}")
+    public ResponseEntity<List<CommentDto>> getAllByApartment_Id(@PathVariable("apartmentid") Integer apartmentId) {
         // log.info("getAllByApartment_Id , CommentController");
         apartmentService.getById(apartmentId);
         return new ResponseEntity<>(commentService.getAllByApartment_Id(apartmentId), HttpStatus.OK);
